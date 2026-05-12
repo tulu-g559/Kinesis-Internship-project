981ba32b8fd11fd1285bba169bbbff0b7ed045c3
tulu-g559 <garnab559@gmail.com>
2026-05-12 15:45:52 +0530

    user profile

---
+++ server/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_cors import CORS
+
+from app.config import Config
+from app.extensions import db, jwt, bcrypt
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    CORS(app)
+
+    db.init_app(app)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
+
+    from app.auth.routes import auth_bp
+    app.register_blueprint(auth_bp, url_prefix="/api/auth")
+
+    return app
---
+++ server/app/models/user.py
@@ -0,0 +1,15 @@
+from app.extensions import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(50), nullable=False)
+
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    password = db.Column(db.String(255), nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
